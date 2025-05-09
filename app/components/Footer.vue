e25d35c3ea061616e6258d81ea0b09f06cf09f64
Railson Albino <railsonvlogz@gmail.com>
2025-05-08 21:53:40 -0300

    ♻️ add footer component with copyright information

---
+++ app/components/Footer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="bg-slate-800 p-4 text-center text-white">
+    <div class="flex items-center justify-center pt-10 font-extralight text-white md:mt-6 md:pt-5">
+      a
+    </div>
+    <p class="pt-5 font-extralight md:text-sm">
+      Desenvolvido por Railson Albino
+    </p>
+    <p class="pt-2 font-extralight md:text-sm">
+      © 2025 Todos os direitos reservados
+    </p>
+  </footer>
+</template>
